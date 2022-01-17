8b010b2f2a73ade0bf89b05b764b639766bf26a9
zangzangs <kimjangh94@gmail.com>
2022-01-17 23:31:49 +0900

    Initial commit

---
+++ mobility/src/test/java/cardiff/mobility/MobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package cardiff.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
