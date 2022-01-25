97750865897ada5577d8352b16e8c6a8b58e6b94
zangzangs <kimjangh94@gmail.com>
2022-01-25 21:46:06 +0900

    관리자 화면 html css js 기본 템플릿 최초생성

--- cardiff-admin-module/src/main/java/cardiff/admin/controller/HomeController.java
+++ cardiff-admin-module/src/main/java/cardiff/admin/controller/HomeController.java
@@ -2,9 +2,14 @@ package cardiff.admin.controller;
 
 import cardiff.domain.admin.Admin;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    Admin admin = new Admin();
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
 }
