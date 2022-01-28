27984155f505875cf314c036cc1abb9b5774947f
zangzangs <kimjangh94@gmail.com>
2022-01-28 21:41:38 +0900

    feat: domain 모듈 스캔을 위한 @EntityScan, @Enable Repositories 추가

--- cardiff-admin-module/src/main/java/cardiff/admin/AdminApplication.java
+++ cardiff-admin-module/src/main/java/cardiff/admin/AdminApplication.java
@@ -2,8 +2,12 @@ package cardiff.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "cardiff.*")
+@SpringBootApplication(scanBasePackages = {"cardiff.domain.*" })
+@EntityScan("cardiff.domain.*")
+@EnableJpaRepositories("cardiff.domain.*")
 public class AdminApplication {
 
 	public static void main(String[] args) {
