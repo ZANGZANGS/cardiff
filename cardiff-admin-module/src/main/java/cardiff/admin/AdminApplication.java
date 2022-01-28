eb9ad25f896d26c9c59ecb04c616c24cb0aeafac
zangzangs <kimjangh94@gmail.com>
2022-01-28 21:50:35 +0900

    feat: scanbasepackages 수정

--- cardiff-admin-module/src/main/java/cardiff/admin/AdminApplication.java
+++ cardiff-admin-module/src/main/java/cardiff/admin/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"cardiff.domain.*" })
+@SpringBootApplication(scanBasePackages = {"cardiff.admin.*","cardiff.domain.*"})
 @EntityScan("cardiff.domain.*")
 @EnableJpaRepositories("cardiff.domain.*")
 public class AdminApplication {
