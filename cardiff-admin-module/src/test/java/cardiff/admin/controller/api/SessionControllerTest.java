76e998b32c17c4558b818f48cbbc1134dcb56542
zangzangs <kimjangh94@gmail.com>
2022-03-02 17:11:31 +0900

    feat: security 테스트 코드 작성

--- cardiff-admin-module/src/test/java/cardiff/admin/controller/api/SessionControllerTest.java
+++ cardiff-admin-module/src/test/java/cardiff/admin/controller/api/SessionControllerTest.java
@@ -1,88 +1,106 @@
 package cardiff.admin.controller.api;
 
-import cardiff.admin.dto.AdminLoginDto;
-import cardiff.admin.service.AdminLoginService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
+import cardiff.domain.entity.Admin;
+import cardiff.domain.repository.AdminLoginRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(SessionController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SessionControllerTest {
 
-    @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
     @Autowired
-    ObjectMapper objectMapper;
+    WebApplicationContext webApplicationContext;
 
-    @MockBean
-    AdminLoginService adminLoginService;
+    @Autowired
+    AdminLoginRepository repository;
 
-    final AdminLoginDto.Req req = AdminLoginDto.Req.builder()
-            .name("테스트관리자")
-            .password("1234")
-            .build();
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+
+    }
 
     @Test
     @DisplayName("로그인 성공 테스트")
+    @Transactional
     void 로그인_성공_테스트() throws Exception{
         // given
+        String username = "테스트관리자";
+        String password = "12345";
 
-        given(adminLoginService.doLogin(any(), any())).willReturn(true);
+        Admin admin = new Admin(username, password);
+        admin.passwordEncoding(passwordEncoder);
+        repository.save(admin);
 
         // when // then
-        mvc.perform(post("/api/v1/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(req))
-                .accept(MediaType.APPLICATION_JSON)
-                .characterEncoding("UTF-8")
+        HttpSession session = mvc.perform(formLogin()
+                        .user(username)
+                        .password(password)
                 )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true));
+                .andExpect(status().is3xxRedirection())
+                .andReturn().getRequest().getSession();
+
+        Assertions.assertThat(session.getAttribute(session.getAttributeNames().nextElement()).toString())
+                .contains("테스트관리자");
+
     }
 
     @Test
     @DisplayName("로그인 실패 테스트")
+    @Transactional
     void 로그인_실패_테스트() throws Exception{
         // given
-        given(adminLoginService.doLogin(any(), any())).willReturn(false);
+        String username = "가짜관리자";
+        String password = "12345";
 
         // when // then
-        mvc.perform(post("/api/v1/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(req))
-                .accept(MediaType.APPLICATION_JSON)
-                .characterEncoding("UTF-8")
+        HttpSession session = mvc.perform(formLogin()
+                        .user(username)
+                        .password(password)
                 )
-                .andExpect(status().isAccepted())
-                .andExpect(jsonPath("$.success").value(false))
-                .andExpect(content().string(Matchers.containsString("errorMessage")));
+
+                .andExpect(status().is3xxRedirection())
+                .andReturn().getRequest().getSession();
+
+        Assertions.assertThat(session.getAttribute(session.getAttributeNames().nextElement()).toString())
+                .contains("BadCredentialsException");
 
     }
 
     @Test
     @DisplayName("로그아웃 테스트")
+    @WithMockUser(roles="ADMIN")
     void 로그아웃_테스트() throws Exception{
 
-        mvc.perform(delete("/api/v1/logout")
-                .contentType(MediaType.APPLICATION_JSON)
-                .session(new MockHttpSession())
-                )
-                .andExpect(status().isOk());
+        mvc.perform(logout())
+                .andExpect(status().is3xxRedirection())
+                .andReturn().getRequest().getSession();
 
     }
 }
